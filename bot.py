dffab4ae8ebfdd7c1c1743f53c0d98ed782b082f
RandyRodriguez <a.ya.miheev@gmail.com>
2018-02-19 19:19:40 +0300

    Change logging; change format of camera info; add a couple of todo's; open db.connection when bot starts; add a couple of logs

--- bot.py
+++ bot.py
@@ -12,17 +12,24 @@ import exifread
 import requests
 from io import BytesIO
 import traceback
-import handle_logs
+from handle_logs import log, clean_log_folder
 import language_pack
 import db_connector
 import MySQLdb
 
-logFile, logConsole = handle_logs.set_loggers()  # set up logging via my module
+log.info('Starting photoGPSbot...')
+log.info('Cleaning log folder...')
+clean_log_folder(20)
+
 bot = telebot.TeleBot(config.token)
+# TODO Make command to safely turn bot down when necessary (closing ssh and connection to db)
+# TODO Store language choice of each user not in a freaking global variable which is common for everyone, but in db
 lang = language_pack.language_ru
 
-logConsole.info('Starting photoGPSbot...')
-logFile.info('Starting photoGPSbot...')
+# Connect to db
+db = db_connector.connect()
+if not db:
+    log.warning('Can\'t connect to db.')
 
 
 @bot.message_handler(commands=['language', 'start'])
@@ -52,8 +59,7 @@ def answer_text_message(message):
                                                                                           message.from_user.last_name,
                                                                                           message.from_user.username,
                                                                                           message.from_user.id))
-        logFile.info(log_msg)
-        logConsole.info(log_msg)
+        log.info(log_msg)
 
 
 @bot.message_handler(content_types=['photo'])
@@ -65,8 +71,16 @@ def answer_photo_message(message):
                                               message.from_user.username,
                                               message.from_user.id))
 
-    logFile.info(log_message)
-    logConsole.info(log_message)
+    log.info(log_message)
+
+
+def dedupe_string(string):
+    splitted_string = string.split(' ')
+    deduped_string = ''
+    for x in splitted_string:
+        if x not in deduped_string:
+            deduped_string += x + ' '
+    return deduped_string.rstrip()
 
 
 def exif_to_dd(data):
@@ -79,18 +93,12 @@ def exif_to_dd(data):
         lon_ref = str(data['GPS GPSLongitudeRef'])
         lon = data['GPS GPSLongitude']
     except KeyError:
-        logFile.info('This picture doesn\'t contain coordinates.')
-        logConsole.info('This picture doesn\'t contain coordinates.')
+        log.info('This picture doesn\'t contain coordinates.')
         return [lang['no_gps']]
         # TODO Save exif of photo if converter catch an error trying to convert gps data
 
     # convert ifdtag from exifread module to decimal degree format of coordinate
     def idf_tag_to_coordinate(tag):
-
-        logFile.debug('Latitude reference: ' + lat_ref)
-        logFile.debug('Latitude: ' + str(lat))
-        logFile.debug('Longitude reference: ' + lon_ref)
-        logFile.debug('Longitude: ' + str(lon))
 
         tag = str(tag).replace('[', '').replace(']', '').split(',')
         if '/' in tag[2]:
@@ -100,8 +108,7 @@ def exif_to_dd(data):
             # Rare case so far - when there is just a number
             tag[2] = int(tag[2])
         else:
-            logFile.warning('Can\'t read gps from file!')
-            logConsole.warning('Can\'t read gps from file!')
+            log.warning('Can\'t read gps from file!')
             return False
 
         return int(tag[0]) + int(tag[1]) / 60 + tag[2] / 3600
@@ -118,22 +125,16 @@ def exif_to_dd(data):
 # Save camera info to database to collect statistics
 def save_camera_info(data):
 
-    tables = ['camera_brand_stat', 'camera_model_stat', 'lens_brand_stat', 'lens_model_stat']
-    columns = ['camera_brand', 'camera_model', 'lens_brand', 'lens_model']
+    tables = ['camera_name_table', 'lens_name_table']
+    columns = ['camera_name', 'lens_name']
 
-    # Connect to db
-    db = db_connector.connect()
-    if not db:
-        logFile.warning('Can\'t connect to db.')
-        logConsole.warning('Can\'t connect to db.')
-        return
     cursor = db.cursor()
 
-    logConsole.debug('############# debug info about storing exif to db ################')
+    log.debug('############# debug info about storing exif to db ################')
     for tag, table, column in zip(data, tables, columns):
 
-        logConsole.debug('Name: {}; Table: {}; Column: {} from EXIF'.format(tag, table, column))
-        if tag is not None:  # if there was this information inside EXIF of the photo
+        log.debug('Name: {}; Table: {}; Column: {} from EXIF'.format(tag, table, column))
+        if tag:  # if there was this information inside EXIF of the photo
             tag = str(tag).strip()
 
             # Collate with special table if there more appropriate name for the tag
@@ -143,18 +144,15 @@ def save_camera_info(data):
                 row = cursor.execute(query)
                 if row:
                     tag = cursor.fetchone()[0]  # Get appropriate tag from the table
-                    logConsole.debug('Tag after looking up in tag_tables - {}.'.format(tag))
-                    logFile.debug('Tag after looking up in tag_tables - {}.'.format(tag))
+                    log.debug('Tag after looking up in tag_tables - {}.'.format(tag))
             except (MySQLdb.Error, MySQLdb.Warning) as e:
-                logConsole.error(e)
-                logFile.error(e)
+                log.error(e)
 
             try:
                 query = 'SELECT id FROM {} WHERE {} = "{}"'.format(table, column, tag)
                 row = cursor.execute(query)
             except (MySQLdb.Error, MySQLdb.Warning) as e:
-                logConsole.error(e)
-                logFile.error(e)
+                log.error(e)
                 return
             if not row:
                 try:
@@ -162,26 +160,20 @@ def save_camera_info(data):
                              'VALUES ("{}", 1);'.format(table, column, tag))
                     cursor.execute(query)
                     db.commit()
-                    logConsole.info('{} was added to {}'.format(tag, table))
-                    logFile.info('{} was added to {}'.format(tag, table))
+                    log.info('{} was added to {}'.format(tag, table))
                 except (MySQLdb.Error, MySQLdb.Warning) as e:
-                    logConsole.error(e)
-                    logFile.error(e)
+                    log.error(e)
             else:
                 try:
-                    logConsole.debug('There is {} in {} already'.format(tag, table))
-                    logFile.debug('There is {} in {} already'.format(tag, table))
+                    log.debug('There is {} in {} already'.format(tag, table))
                     query = 'UPDATE {} SET occurrences = occurrences + 1 WHERE {}="{}"'.format(table, column, tag)
                     cursor.execute(query)
                     db.commit()
-                    logConsole.debug('{} in {} was updated'.format(tag, table))
-                    logFile.debug('{} in {} was updated'.format(tag, table))
+                    log.debug('{} in {} was updated'.format(tag, table))
                 except (MySQLdb.Error, MySQLdb.Warning) as e:
-                    logConsole.error(e)
-                    logFile.error(e)
+                    log.error(e)
 
-    logConsole.debug('############## end of debug info about storing exif to db###############\n')
-    db_connector.disconnect()
+    log.debug('############## end of debug info about storing exif to db###############\n')
 
 
 def read_exif(image):
@@ -189,8 +181,7 @@ def read_exif(image):
     answer = []
     exif = exifread.process_file(image, details=False)
     if len(exif.keys()) < 1:
-        logFile.info('This picture doesn\'t contain EXIF.')
-        logConsole.info('This picture doesn\'t contain EXIF.')
+        log.info('This picture doesn\'t contain EXIF.')
         return False, False
 
     answer.extend(exif_to_dd(exif))
@@ -198,29 +189,29 @@ def read_exif(image):
     # Get necessary tags from EXIF data
 
     date_time = exif.get('EXIF DateTimeOriginal', None)
-    camera_brand = exif.get('Image Make', None)
-    camera_model = exif.get('Image Model', None)
-    lens_brand = exif.get('EXIF LensMake', None)
-    lens_model = exif.get('EXIF LensModel', None)
+    camera_brand = str(exif.get('Image Make', ''))
+    camera_model = str(exif.get('Image Model', ''))
+    lens_brand = str(exif.get('EXIF LensMake', ''))
+    lens_model = str(exif.get('EXIF LensModel', ''))
 
     if not any([date_time, camera_brand, camera_model, lens_brand, lens_model]):
         return False  # Means that there is actually no any data of our interest
 
-    camera_info = camera_brand, camera_model, lens_brand, lens_model
+    # TODO Check tags against mysql db
+    # TODO Store proper tag in mysql (now it's broken)
+    # to send it to database after bot answers to user
 
-    date_time_str = lang['camera_info'][0] + ': ' + str(date_time) + '\n' if date_time is not None else None
-    camera_brand_str = lang['camera_info'][1] + ': ' + str(camera_brand) + '\n' if camera_brand is not None else None
-    camera_model_str = lang['camera_info'][2] + ': ' + str(camera_model) + '\n' if camera_model is not None else None
-    lens_brand_str = lang['camera_info'][3] + ': ' + str(lens_brand) + '\n' if lens_brand is not None else None
-    lens_model_str = lang['camera_info'][4] + ': ' + str(lens_model) + '\n' if lens_model is not None else None
-
-    # Haven't done it yet completely
-    # save_camera_info(camera_brand, camera_model, lens_brand, lens_model)
+    # Make user message about camera from exif
+    date_time_str = str(date_time) if date_time is not None else None
+    camera = dedupe_string(camera_brand + ' ' + camera_model) if camera_brand + ' ' + camera_model != ' ' else None
+    lens = dedupe_string(lens_brand + ' ' + lens_model) if lens_brand + ' ' + lens_model != ' ' else None
+    camera_info = camera, lens
 
     info_about_shot = ''
-    for item in [date_time_str, camera_brand_str, camera_model_str, lens_brand_str, lens_model_str]:
-        if item is not None:
-            info_about_shot += item
+    for tag, item in zip(lang['camera_info'], [date_time_str, camera, lens]):
+        if item:
+            print()
+            info_about_shot += tag + item + '\n'
 
     answer.append(info_about_shot)
     return answer, camera_info
@@ -234,8 +225,7 @@ def handle_image(message):
                                                                                          message.from_user.username,
                                                                                          message.from_user.id))
 
-    logFile.info(log_msg)
-    logConsole.info(log_msg)
+    log.info(log_msg)
 
     # get image
     file_id = bot.get_file(message.document.file_id)
@@ -259,8 +249,7 @@ def handle_image(message):
                                       message.from_user.username,
                                       message.from_user.id))
 
-        logFile.info(log_msg)
-        logConsole.info(log_msg)
+        log.info(log_msg)
         save_camera_info(cam_info)
     else:
         bot.send_message(message.chat.id, answer[0] + '\n' + answer[1])
@@ -269,21 +258,19 @@ def handle_image(message):
                                       message.from_user.last_name,
                                       message.from_user.username,
                                       message.from_user.id))
-        logFile.info(log_msg)
-        logConsole.info(log_msg)
+        log.info(log_msg)
         save_camera_info(cam_info)
 
 # error_counter = 0
 # while True:
 #     if error_counter == 30:
-#         logConsole.error('Emergency stop due to loop of polling exceptions')
+#         log.error('Emergency stop due to loop of polling exceptions')
 #         exit()
 #     try:
 #         if __name__ == '__main__':
 #             bot.polling(none_stop=True)  # Keep bot receiving messages
 #     except:
-#         logFile.error('Freaking polling!')
-#         logConsole.error('Freaking polling!')
+#         log.error('Freaking polling!')
 #         error_counter += 1
 
 
@@ -291,8 +278,8 @@ def telegram_polling():
     try:
         bot.polling(none_stop=True, timeout=60)  # Keep bot receiving messages
     except:
-        logFile.warning('Polling issue\n' + traceback.format_exc())
-        logConsole.warning('Polling issue\n' + traceback.format_exc())
+        db_connector.disconnect()
+        log.warning('Polling issue\n' + traceback.format_exc())
         bot.stop_polling()
         # telegram_polling()
 
